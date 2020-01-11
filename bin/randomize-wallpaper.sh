a85a289cf70a10bd54ba3e41fbd4762b8a05d1b2
Brenton Morris <brenton.smorris@gmail.com>
2020-01-11 15:41:32 +1300

    update wallpaper randomize script

--- bin/randomize-wallpaper.sh
+++ bin/randomize-wallpaper.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# Get focused display
+focused=`i3-msg -t get_workspaces | jq '.[] | select(.focused==true).output'`
+
+echo ".$focused."
+if [ "$focused" = "\"eDP-1\"" ]; then
+    focused=0
+else
+    focused=1
+fi
+echo ".$focused."
+
 N=1
 ls $HOME/Pictures/Wallpapers |sort -R |tail -$N |while read file; do
-	nitrogen --set-scaled $HOME/Pictures/Wallpapers/$file
+	nitrogen --force-setter=xinerama --set-scaled --head=$focused $HOME/Pictures/Wallpapers/$file
 done
 
